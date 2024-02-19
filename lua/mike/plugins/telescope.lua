e1e09300d4956ec8e8bcb73d06e2a97897b4974f
mike <mpmccauley15@gmail.com>
2024-02-18 20:03:08 -0500

    added lazygit

--- lua/mike/plugins/telescope.lua
+++ lua/mike/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
